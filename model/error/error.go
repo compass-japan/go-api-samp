ee2c6d220fc060bab48f85df8d47d0fa38d39bfa
ohorigenal <6256coo@gmail.com>
2021-01-19 02:08:33 +0900

    add error def

---
+++ model/error/error.go
@@ -0,0 +1,85 @@
+package error
+
+import "fmt"
+
+/*
+ *  エラーの構造定義
+ *  getterと標準エラー(errors package)のラップ
+ */
+
+type (
+	SystemErrorBuilder      func(cause error, i interface{}) SystemError
+	ApplicationErrorBuilder func(cause error, i interface{}) ApplicationError
+
+	SystemError interface {
+		error
+		Is(error) bool
+		Unwrap() error
+		Message() string
+		Causes() []string
+	}
+
+	ApplicationError interface {
+		SystemError
+		StatusCode() int
+		LogIgnorable() bool
+	}
+
+	systemError struct {
+		message string
+		cause   error
+	}
+
+	applicationError struct {
+		statusCode   int
+		logIgnorable bool
+		*systemError
+	}
+)
+
+func (e *systemError) Error() string {
+	return e.message
+}
+
+func (e *systemError) Is(target error) bool {
+	return e.Error() == target.Error()
+}
+
+func (e *systemError) Unwrap() error {
+	return e.cause
+}
+
+func (e *systemError) Message() string {
+	return e.message
+}
+
+func (e *systemError) Causes() []string {
+	causes := []string{e.Error()}
+
+	cur := e.Unwrap()
+	for cur != nil {
+		causes = append(causes, cur.Error())
+		e, ok := cur.(interface {
+			error
+			Unwrap() error
+		})
+		if !ok {
+			break
+		}
+		cur = e.Unwrap()
+	}
+
+	return causes
+}
+
+func (e *applicationError) Error() string {
+	return fmt.Sprintf("status: %d, e: %s", e.statusCode, e.message)
+}
+
+func (e *applicationError) StatusCode() int {
+	return e.statusCode
+}
+
+func (e *applicationError) LogIgnorable() bool {
+	return e.logIgnorable
+}
---
+++ model/error/errors.go
@@ -0,0 +1,86 @@
+package error
+
+import "net/http"
+
+/*
+ * Application全体とSystem内部で使用するエラーの変数定義
+ */
+
+var (
+	Applicaton = struct {
+		HttpMethodNotAllowedError ApplicationErrorBuilder
+		HttpRouteNotFoundError    ApplicationErrorBuilder
+		UnauthorizedError ApplicationErrorBuilder
+		InvalidRequestParamError       ApplicationErrorBuilder
+		InternalServerError       ApplicationErrorBuilder
+	}{
+		HttpRouteNotFoundError: func(cause error, i interface{}) ApplicationError {
+			return &applicationError{
+				statusCode:   http.StatusNotFound,
+				logIgnorable: true,
+				systemError: &systemError{
+					message: "route not found.",
+					cause:   cause,
+				},
+			}
+		},
+		HttpMethodNotAllowedError: func(cause error, i interface{}) ApplicationError {
+			return &applicationError{
+				statusCode:   http.StatusMethodNotAllowed,
+				logIgnorable: true,
+				systemError: &systemError{
+					message: "http method not allowed.",
+					cause:   cause,
+				},
+			}
+		},
+		UnauthorizedError: func(cause error, i interface{}) ApplicationError {
+			return &applicationError{
+				statusCode:   http.StatusUnauthorized,
+				logIgnorable: false,
+				systemError: &systemError{
+					message: "unauthorized error.",
+					cause:   cause,
+				},
+			}
+		},
+		InvalidRequestParamError: func(cause error, i interface{}) ApplicationError {
+			return &applicationError{
+				statusCode:   http.StatusBadRequest,
+				logIgnorable: false,
+				systemError: &systemError{
+					message: "invalid request param.",
+					cause:   cause,
+				},
+			}
+		},
+		InternalServerError: func(cause error, i interface{}) ApplicationError {
+			return &applicationError{
+				statusCode:   http.StatusInternalServerError,
+				logIgnorable: false,
+				systemError: &systemError{
+					message: "internal server error.",
+					cause:   cause,
+				},
+			}
+		},
+	}
+
+	System = struct {
+		UnknownSystemError SystemErrorBuilder
+		DataStoreError     SystemErrorBuilder
+	}{
+		UnknownSystemError: func(cause error, i interface{}) SystemError {
+			return &systemError{
+				message: "unknown system error.",
+				cause:   cause,
+			}
+		},
+		DataStoreError: func(cause error, i interface{}) SystemError {
+			return &systemError{
+				message: "datastore error.",
+				cause:   cause,
+			}
+		},
+	}
+)
