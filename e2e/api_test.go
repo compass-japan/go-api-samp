6bbe225013056ed765209866617398cb1460f218
ohorigenal <6256coo@gmail.com>
2021-01-29 23:47:51 +0900

    add e2e

---
+++ e2e/api_test.go
@@ -0,0 +1,81 @@
+// +build e2e
+
+package e2e
+
+import (
+	"github.com/go-resty/resty/v2"
+	"github.com/labstack/echo"
+	"github.com/stretchr/testify/assert"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+)
+
+const (
+	authHeader = "Auth-Token"
+	token = "auth-token"
+	E2E_ENDPOINT = "E2E_ENDPOINT"
+)
+
+var (
+	url = ""
+)
+
+func TestE2E(t *testing.T) {
+	t.Helper()
+	if os.Getenv(E2E_ENDPOINT) == "" {
+		t.Logf("not set E2E_ENDPOINT env")
+		t.FailNow()
+	}
+	url = os.Getenv(E2E_ENDPOINT)
+
+	tests := []struct{
+		name string
+		test func(t *testing.T)
+	}{
+		{
+			name: "ヘルスチェック",
+			test: testHealthCheck,
+		},
+	}
+
+	for _, test := range tests {
+		test := test
+		t.Run(test.name, test.test)
+	}
+}
+
+func testHealthCheck(t *testing.T) {
+	tests := []struct{
+		name string
+		path string
+	}{
+		{
+			name: "ヘルスチェック",
+			path: "/health",
+		},
+	}
+
+	client := resty.New()
+	client.RetryCount = 3
+	client.RetryWaitTime = 1 * time.Second
+	// client.SetTLSClientConfig(&tls.Config{InsecureSkipVerify: true})
+
+	for _, test := range tests {
+		tp := test
+		t.Run(tp.name, func(t *testing.T) {
+			res, err := client.R().
+				SetHeader(authHeader, token).
+				SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+				Get(url + tp.path)
+
+			if err != nil {
+				t.Error(err)
+			}
+
+			t.Log(string(res.Body()))
+			assert.Equal(t, http.StatusOK, res.StatusCode())
+		})
+	}
+}
--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.15
 require (
 	github.com/DATA-DOG/go-sqlmock v1.5.0
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+	github.com/go-resty/resty/v2 v2.4.0
 	github.com/go-sql-driver/mysql v1.5.0
 	github.com/google/uuid v1.1.5
 	github.com/jinzhu/configor v1.2.1
@@ -14,7 +15,6 @@ require (
 	github.com/stretchr/testify v1.4.0
 	github.com/valyala/fasttemplate v1.2.1 // indirect
 	golang.org/x/crypto v0.0.0-20201208171446-5f87f3452ae9 // indirect
-	golang.org/x/net v0.0.0-20201209123823-ac852fbbde11 // indirect
 	golang.org/x/sys v0.0.0-20201211090839-8ad439b19e0f // indirect
 	golang.org/x/text v0.3.4 // indirect
 )
