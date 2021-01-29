92074f55dfd5cc180c500d5c1864a16ee2e0b204
ohorigenal <6256coo@gmail.com>
2021-01-29 11:17:41 +0900

    add middleware

--- controller/internal/middleware.go
+++ controller/internal/middleware.go
@@ -4,7 +4,9 @@ import (
 	"context"
 	"github.com/google/uuid"
 	"github.com/labstack/echo"
+	"go-api-samp/model/dto"
 	"go-api-samp/util/scope"
+	"net/http"
 )
 
 func getContext(eCtx echo.Context) context.Context {
@@ -14,14 +16,28 @@ func getContext(eCtx echo.Context) context.Context {
 	return context.Background()
 }
 
-func SetContext() echo.MiddlewareFunc {
-	return func(next echo.HandlerFunc) echo.HandlerFunc {
-		return func(eCtx echo.Context) error {
-			ctx := eCtx.Request().Context()
-			id, _ := uuid.NewRandom()
-			ctx = scope.SetRequestID(ctx, id.String())
-			eCtx.Set(string(scope.RequestIDContextKey), ctx)
-			return next(eCtx)
+func Handler (next echo.HandlerFunc) echo.HandlerFunc{
+	return func (eCtx echo.Context) error {
+		token := eCtx.Request().Header.Get("Auth-Token")
+		if token != "auth-token" {
+			return eCtx.JSON(http.StatusUnauthorized, &dto.ErrorResponse{
+				Message: "Unauthorized request",
+			})
 		}
+		return next(eCtx)
 	}
+}
+
+func SetContext(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(eCtx echo.Context) error {
+		ctx := eCtx.Request().Context()
+		id, _ := uuid.NewRandom()
+		ctx = scope.SetRequestID(ctx, id.String())
+		eCtx.Set(string(scope.RequestIDContextKey), ctx)
+		return next(eCtx)
+	}
+}
+
+func ErrorHandler(err error, eCtx echo.Context) {
+
 }
