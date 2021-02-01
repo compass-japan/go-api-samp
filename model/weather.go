268625bb3f60c4cbb2ce6e8a5f4d2e68d7eedca6
ohorigenal <6256coo@gmail.com>
2021-02-01 21:28:47 +0900

    add method ToWeather

--- model/weather.go
+++ model/weather.go
@@ -1,8 +1,27 @@
 package model
 
+type wtr uint16
+
 const (
-	Sunny = iota
+	Sunny wtr = iota + 1
 	Cloudy
 	Rainy
 	Snowy
 )
+
+var (
+	wtrMap = map[wtr]string{
+		0: "",
+		1: "Sunny",
+		2: "Cloudy",
+		3: "Rainy",
+		4: "Snowy",
+	}
+)
+
+func ToWeather(i int) string {
+	if v, ok := wtrMap[wtr(i)]; ok {
+		return v
+	}
+	return ""
+}
