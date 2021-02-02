f420cd6d0e48c2c64ed116af7346b9917e7e13ac
ohorigenal <6256coo@gmail.com>
2021-02-03 03:44:07 +0900

    add minikube deploy

---
+++ .k8s/overlay/minikube/db/init/create.sql
@@ -0,0 +1,30 @@
+DROP DATABASE IF EXISTS goapi;
+CREATE DATABASE IF NOT EXISTS goapi;
+USE goapi;
+
+DROP TABLE IF EXISTS WEATHER;
+CREATE TABLE IF NOT EXISTS WEATHER
+(
+dat char(8) NOT NULL,
+weather int NOT NULL,
+location_id int NOT NULL,
+comment varchar(255),
+PRIMARY KEY(dat, location_id)
+);
+
+INSERT INTO WEATHER
+(
+dat, weather, location_id, comment
+)
+VALUES
+("20200101", 1, 1, "test comment");
+
+DROP TABLE IF EXISTS LOCATION;
+CREATE TABLE IF NOT EXISTS LOCATION
+(
+id int PRIMARY KEY,
+city varchar(20) NOT NULL
+);
+
+INSERT INTO LOCATION VALUES
+(1, "新宿");
