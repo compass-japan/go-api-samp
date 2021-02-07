ccf686ca335e90d7e1725f9778a1fb1d5802e7ad
ohorigenal <6256coo@gmail.com>
2021-02-07 19:26:58 +0900

    add e2e test

--- e2e/api_test.go
+++ e2e/api_test.go
@@ -6,10 +6,13 @@ import (
 	"github.com/go-resty/resty/v2"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
+	"go-api-samp/model/dto"
 	"net/http"
 	"os"
+	"strconv"
 	"testing"
 	"time"
+	"encoding/json"
 )
 
 /*
@@ -27,6 +30,7 @@ const (
 
 var (
 	url = ""
+	client *resty.Client = nil
 )
 
 func TestE2E(t *testing.T) {
@@ -37,6 +41,10 @@ func TestE2E(t *testing.T) {
 	}
 	url = os.Getenv(E2E_ENDPOINT)
 
+	c := resty.New()
+	c.SetTimeout(5 * time.Second)
+	client = c
+
 	tests := []struct{
 		name string
 		test func(t *testing.T)
@@ -44,6 +52,14 @@ func TestE2E(t *testing.T) {
 		{
 			name: "ヘルスチェック",
 			test: testHealthCheck,
+		},
+		{
+			name: "外部API",
+			test: testExAPI,
+		},
+		{
+			name: "Get Weather",
+			test: testGetWeather,
 		},
 	}
 
@@ -60,7 +76,6 @@ func testHealthCheck(t *testing.T) {
 	}{
 		{
 			name: "ヘルスチェック",
-			path: "/health",
 		},
 	}
 
@@ -75,14 +90,161 @@ func testHealthCheck(t *testing.T) {
 			res, err := client.R().
 				SetHeader(authHeader, token).
 				SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
-				Get(url + tp.path)
+				Get(url + "/health")
 
 			if err != nil {
 				t.Error(err)
 			}
 
-			t.Log(string(res.Body()))
 			assert.Equal(t, http.StatusOK, res.StatusCode())
+			assert.Equal(t, "{ Status: OK }", res.String())
+		})
+	}
+}
+
+func testExAPI(t *testing.T) {
+	tests := []struct{
+		name string
+	}{
+		{
+			name: "正常系",
+		},
+	}
+
+	for _, test := range tests {
+		tp := test
+		t.Run(tp.name, func(t *testing.T) {
+			res, err := client.R().
+				SetHeader(authHeader, token).
+				SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+				Get(url + "/get/apidata")
+
+			assert.NoError(t, err)
+			assert.Equal(t, http.StatusOK, res.StatusCode())
+			t.Log(res.String())
+			assert.NotEmpty(t, res.String())
+		})
+	}
+}
+
+func testGetWeather(t *testing.T) {
+	var get = func(locationId int, date string, isErr bool, status int, expected *dto.GetWeatherResponse) func() {
+		return func(){
+			res, err := client.R().
+				SetHeader(authHeader, token).
+				SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+				Get(url + "/get/" + strconv.Itoa(locationId) + "/" + date)
+
+			if isErr {
+				assert.Equal(t, status, res.StatusCode())
+			}
+			if !isErr {
+				assert.NoError(t, err)
+				assert.Equal(t, http.StatusOK, res.StatusCode())
+				b, _ := json.Marshal(expected)
+				assert.JSONEq(t, string(b), res.String())
+			}
+			t.Log(locationId, date, expected)
+		}
+	}
+	var register = func(isErr bool, status int, request *dto.RegisterRequest) func() {
+		return func(){
+			res, err := client.R().
+				SetHeader(authHeader, token).
+				SetHeader(echo.HeaderContentType, echo.MIMEApplicationJSON).
+				SetBody(request).
+				Post(url + "/register")
+
+			if isErr {
+				assert.Equal(t, status, res.StatusCode())
+			}
+			if !isErr {
+				assert.NoError(t, err)
+				assert.Equal(t, http.StatusOK, res.StatusCode())
+				assert.JSONEq(t, `{"message": "weather registered"}`, res.String())
+			}
+			t.Log(request)
+		}
+	}
+	tests := []struct{
+		name string
+		functions []interface{}
+	}{
+		{
+			name: "register(insert), get, register(update), get",
+			functions: []interface{}{
+				register(false, http.StatusOK, &dto.RegisterRequest{
+					LocationId: 1,
+					Date: "20001010",
+					Weather: 3,
+					Comment: "register(insert) comment",
+				}),
+				get(1, "20001010", false, http.StatusOK, &dto.GetWeatherResponse{
+					Location: "新宿",
+					Date: "20001010",
+					Weather: "Rainy",
+					Comment: "register(insert) comment",
+				}),
+				register(false, http.StatusOK, &dto.RegisterRequest{
+					LocationId: 1,
+					Date: "20001010",
+					Weather: 1,
+					Comment: "register(update) comment",
+				}),
+				get(1, "20001010", false, http.StatusOK, &dto.GetWeatherResponse{
+					Location: "新宿",
+					Date: "20001010",
+					Weather: "Sunny",
+					Comment: "register(update) comment",
+				}),
+			},
+		},
+		{
+			name: "register error",
+			functions: []interface{}{
+				register(true, http.StatusBadRequest, &dto.RegisterRequest{
+					LocationId: 0, // not required
+					Date: "20001010",
+					Weather: 1,
+					Comment: "no register",
+				}),
+				register(true, http.StatusInternalServerError, &dto.RegisterRequest{
+					LocationId: 999, // no location
+					Date: "20001010",
+					Weather: 1,
+					Comment: "no register",
+				}),
+				register(true, http.StatusBadRequest, &dto.RegisterRequest{
+					LocationId: 1,
+					Date: "2000101", // len != 8
+					Weather: 1,
+					Comment: "no register",
+				}),
+				register(true, http.StatusBadRequest, &dto.RegisterRequest{
+					LocationId: 1,
+					Date: "20001010",
+					Weather: 0, // not required
+					Comment: "no register",
+				}),
+			},
+		},
+		{
+			name: "get error",
+			functions: []interface{}{
+				get(0, "20001010", true, http.StatusBadRequest,nil),
+				get(999, "20001010", true, http.StatusInternalServerError, nil),
+				get(1, "2000101", true, http.StatusBadRequest, nil),
+			},
+		},
+	}
+
+	for _, test := range tests {
+		tp := test
+		t.Run(tp.name, func(t *testing.T) {
+			for _, f := range tp.functions {
+				v, _ := f.(func())
+				v()
+			}
 		})
 	}
 }
